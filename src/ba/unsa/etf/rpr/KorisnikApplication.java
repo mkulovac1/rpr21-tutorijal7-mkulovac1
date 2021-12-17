7f8c00fe7fd37ee5b9c0f4d3990a51ee7145b81a
Merim <mkulovac1@etf.unsa.ba>
2021-12-17 19:55:28 +0100

    Napravljen design

--- src/ba/unsa/etf/rpr/KorisnikApplication.java
+++ src/ba/unsa/etf/rpr/KorisnikApplication.java
@@ -7,13 +7,17 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
 public class KorisnikApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(KorisnikApplication.class.getResource("/fxml/korisnik.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), USE_COMPUTED_SIZE , USE_COMPUTED_SIZE);
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        stage.setMinWidth(600);
+        stage.setMinHeight(400);
         stage.show();
     }
 
